7bdab3473f3c4c0683dfd4122fa83c55f253e12c
Liban <liban.mahdi123@gmail.com>
2024-09-21 01:33:01 -0400

    test 3

--- js/index.js
+++
@@ -1,4 +0,0 @@
-let toggleMenu = () => {
-    let menu = document.getElementById("nav-menu")    
-    menu.style.display = menu.style.display === "none" || menu.style.display === ""? "flex": "none"
-}
